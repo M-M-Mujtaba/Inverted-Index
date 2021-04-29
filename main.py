830a0fbafa3605615052e8f47978c5dfe9f87372
M-M-Mujtaba <l174144@lhr.nu.edu.pk>
2021-04-29 12:39:57 +0000

    testing system

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ from nltk.stem.snowball import SnowballStemmer
 #from nltk.wordnet import WordNetLemmatizer
 from tqdm import tqdm
 from math import sqrt
+import fasttokenizer
+from mosestokenizer import MosesTokenizer
+from execution_time import ExecutionTime
+import re
 
+my_re = re.compile("[a-zA-Z]+-[a-zA-Z]+|[a-zA-Z]+'{0,1}[a-zA-Z]{0,1}")
+
+
+e = ExecutionTime()
+tokenizer = MosesTokenizer('en')
+segmenter = fasttokenizer.Segmenter()
+
+
+#global 
 stop_list = []
 stemmer = SnowballStemmer(language ='english')
 
@@ -15,29 +28,30 @@ def document_parser(document):
 
     return parsed_document
 
-
+@e.timeit
 def my_tokenizer(document):
 
     token_positioning = {}
     positions_list = []
-    for index, token in enumerate(tokenize(document)):
-        kind, text, _ = token
-        text = text.lower()
-        if kind == TOK.WORD:
-            if text not in stop_list:
-                text = stemmer.stem(text)
-                if token_positioning.get(text, False):
-                    token_positioning[text]["positioning"].append(index - token_positioning[text]["positioning"][-1])
-                    token_positioning[text]["tf"] += 1
-                else:
-                    token_positioning[text] = {"positioning":[index] , "tf":1}
+    for index, token in enumerate(my_re.findall(document)):
+        text = token.lower()
+
+        if text not in stop_list:
+            text = stemmer.stem(text)
+            if token_positioning.get(text, False):
+                token_positioning[text]["positioning"].append(index - token_positioning[text]["positioning"][-1])
+                token_positioning[text]["tf"] += 1
+            else:
+                token_positioning[text] = {"positioning":[index] , "tf":1}
+
     return token_positioning
 
 
 def pre_processing(document):
 
     document = document_parser(document)
-    tokens = my_tokenizer(document)
+    tokens = [None] * 9068 
+
     return tokens
 
 def store_doc_info(dir, doc_id, doc_name,tokens):
@@ -46,13 +60,16 @@ def store_doc_info(dir, doc_id, doc_name,tokens):
     sum_squares = sum(tf*tf for tf in tok_freqs)
     doc_len = sum(tok_freqs)
     doc_mag = sqrt(sum_squares)
-    print("here")
+    #print("here")
 
     with open("Inverted-Index/Docinfo.txt", "a+") as doc_info:
         doc_info.write(f"{doc_id},{dir}/{doc_name},{doc_len},{doc_mag}\n")
     doc_info.close()
 
-
+@e.timeit
+def token_time(func, file):
+    tokens = func(file) 
+    return my_re.findall(tokens)
 
 
 if __name__ == "__main__":
@@ -65,11 +82,21 @@ if __name__ == "__main__":
         for document in tqdm(listdir(folder + dirs)):
             with open (f"{folder + dirs}/{document}") as doc:
                 # print(f"{folder + dirs}/{document}")
-                try:
+
                     tokens = pre_processing(doc.read())
                     store_doc_info(dirs, doc_id, document, tokens)
-                except:
-                    print("opsie")
+
             doc_id+=1
 
+
+
+    # file_list = listdir("1")
+    # file_name = file_list[136]
+    # with open ("1/" + file_name) as doc:
+    #     file = document_parser(doc.read())
+    # doc.close()
+    # tokens = token_time(segmenter.normalize_and_segment, file)#my_tokenizer(file)
+    # print(tokens)
+    # print(len(tokens))
+    # print(e.logtime_data)
 
