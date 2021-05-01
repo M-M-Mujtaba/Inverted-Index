0e31c7e510e839a53ca187bfd9fbf58b8f42d123
Mujtaba <l174144@lhr.nu.edu.pk>
2021-05-01 13:48:19 -0700

    added function for creating inverted index and merging them + modified stoplist.txt to include constant words

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ import copy
 
 my_re = re.compile("[a-zA-Z]+-{0,1}'{0,1}[a-zA-z]*")
 
-query = ["information", "retrieval", "cs"]
 
-temp = copy.deepcopy(query)
-ans = open("expected_answer.txt", "a")
 
 # global
 
@@ -82,48 +79,73 @@ def document_parser(document: str):
     soup = BeautifulSoup(document)
     parsed_document = soup.get_text()
 
+
     return parsed_document
 
 
 def my_tokenizer(document: str):
     token_positioning = {}
-    global temp
     global stop_list
 
     for index, token in enumerate(my_re.findall(document)):
         text = token.lower()
 
-        if not stop_list.get(text, False):
-            if text in temp:
-                temp.remove(text)
+        if not (stop_list.get(text, False) or stop_list.get(stemmer.stem(text))):
 
             text = stemmer.stem(text)
             if token_positioning.get(text, False):
-                token_positioning[text]["positioning"].append(index - token_positioning[text]["positioning"][-1])
+                token_positioning[text]["positions"].append(index - token_positioning[text]["positions"][-1])
                 token_positioning[text]["tf"] += 1
             else:
-                token_positioning[text] = {"positioning": [index], "tf": 1}
+                token_positioning[text] = {"positions": [index], "tf": 1}
 
     return token_positioning
 
 
-def pre_processing(document: str, doc_id: int):
-    global temp
-    global ans
-    global query
-    document = document_parser(document)
+def pre_processing(document: str):
 
+    document = document_parser(document)
     tokens = my_tokenizer(document)
-    if len(temp) < len(query):
-        ans.write(f"Doc_ID,{doc_id} ")
-        for element in list(set(query)-set(temp)):
-            ans.write(f",{element}")
-        ans.write("\n")
-    temp = copy.deepcopy(query)
+
     return tokens
 
 
-def store_doc_info(doc_info, dir: str, doc_id: int, doc_name: str, tokens: dict):
+# create inverted index from a single document
+def inverter(tokens, doc_id):
+    inverted_index = {}
+    for key in tokens.keys():
+        inverted_index[key] = {}
+        inverted_index[key][doc_id] = tokens[key]
+        inverted_index[key]["df"] = 1
+    inverted_index = dict(sorted(inverted_index.items()))
+
+    return inverted_index
+
+
+def merge_index(inverted_index, invt_ind):
+    if not inverted_index:
+        return  invt_ind
+    else:
+        for key in invt_ind.keys():
+            if inverted_index.get(key, False):
+                for doc_id in invt_ind[key].keys():
+                    if doc_id != 'df':
+                        if inverted_index[key].get(doc_id, False):
+
+                            inverted_index[key][doc_id]["tf"] += invt_ind[key][doc_id]["tf"]
+                            inverted_index[key][doc_id]["positions"].extend(invt_ind[key][doc_id]["positions"])
+
+                        else:
+                            inverted_index[key][doc_id] = invt_ind[key][doc_id]
+                            inverted_index[key]["df"] += 1
+            else:
+                inverted_index[key] = invt_ind[key]
+    inverted_index = dict(sorted(inverted_index.items()))
+
+    return inverted_index
+
+
+def store_doc_info(doc_info,directory: str, doc_name: str, tokens: dict):
     tok_freqs = [token_info["tf"] for token_info in tokens.values()]
     sum_squares = sum(tf * tf for tf in tok_freqs)
     doc_len = sum(tok_freqs)
@@ -152,36 +174,34 @@ def intersect(posting1: iter, posting2: iter):
     return common_documents
 
 
-if __name__ == "__main__":
+# Takes a directory of input files and creates an inverted index for them
+def create_inverted_index(directory, doc_id ):
+    # with open("Docinfo.txt", "a+") as doc_info:
+    inverted_index = {}
+    documents = listdir(directory)
+    for document in tqdm(documents[:10]):
+        with open(f"{directory}/{document}") as file:
+            # print(f"{folder + dirs}/{document}")
+            try:
+                doc = file.read()
+            except:
+                print("file reading opsie")
+                continue
 
-    dir_names = ["1", "2", "3"]
+            tokens = pre_processing(doc)
+            inverted = inverter(tokens, doc_id)
+            inverted_index = merge_index(inverted_index, inverted)
+        # store_doc_info(doc_info, dirs, doc_id, document, tokens)
+
+        doc_id += 1
+
+    return inverted_index, doc_id
+    # doc_info.close()
+
+
+if __name__ == "__main__":
     doc_id = 1
     doc = ""
     populate_stoplist("stoplist.txt")
-
-    with open("Docinfo.txt", "a+") as doc_info:
-        for dirs in dir_names:
-            for document in tqdm(listdir(dirs)):
-                with open(f"{dirs}/{document}") as file:
-                    # print(f"{folder + dirs}/{document}")
-                    try:
-                        doc = file.read()
-                    except:
-                        print("file reading opsie")
-                        continue
-                    tokens = pre_processing(doc, doc_id)
-                    store_doc_info(doc_info, dirs, doc_id, document, tokens)
-
-                doc_id += 1
-    doc_info.close()
-
-
-    # file_list = listdir("1")
-    # file_name = file_list[136]
-    # with open ("1/" + file_name) as doc:
-    #     file = document_parser(doc.read())
-    # doc.close()
-    # tokens = token_time(segmenter.normalize_and_segment, file)#my_tokenizer(file)
-    # print(tokens)
-    # print(len(tokens))
-    # print(e.logtime_data)
+    index , doc_id= create_inverted_index("1", doc_id)
+    print(index)
